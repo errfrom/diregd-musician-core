acce5a94ebf07772d1783ae385671509ff773fdd
Dzmitry Shuisky <errfrom@yandex.ru>
2018-10-04 22:03:45 +0300

    Major changes.
    Code was refactored and seperated into modules.
    Several issues encountered about the attempt to
    generalize logging functionality, so they to be fixed.

--- src/Core.hs
+++ src/Core.hs
@@ -1,17 +1,15 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes                   #-}
-{-# LANGUAGE OverloadedStrings, RecordWildCards             #-}
-{-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings            #-}
 
 module Core ( audioDevicesAvailable, main ) where
 
-import qualified Language.C.Inline          as C
-import qualified Foreign.Ptr                as Ptr (plusPtr)
-import           Foreign.Storable                  (sizeOf)
-import qualified Foreign.C.String           as C   (peekCAString)
-import           Foreign.C.Types                   (CLong, CChar)
-import           Control.Monad.IO.Class            (liftIO)
+import qualified Language.C.Inline       as C
+import           Foreign.Storable             (peek)
+import           Foreign.Ptr                  (castPtr)
+import           Control.Monad                (void)
 
-import           Control.Monad.Trans.Resource (ResIO)
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Trans.Resource (ResIO, runResourceT)
 import           Data.Proxy                   (Proxy(Proxy))
 import           Internal.Foreign
 import           Types
@@ -23,99 +21,39 @@ C.include "<stdlib.h>"
 C.include "<string.h>"
 
 
-audioDevicesAvailable :: ResIO (Maybe PaDeviceList)
+audioDevicesAvailable :: ResIO PaDeviceList
 audioDevicesAvailable = do
-  ptrErrorCode  <- malloc (Proxy :: Proxy CLong)
-  ptrDeviceList <- malloc (Proxy :: Proxy ~ Ptr PaDeviceInfo)
-  cResult <- liftIO $ [C.block| int {
-    void phantom_error_handler(const char* file, int line, const char* fun,
-                               int err, const char* fmt,...) { ; };
-    snd_lib_error_set_handler(&phantom_error_handler);
-    Pa_Initialize();
+  (_, ptrDeviceList') <- malloc (Proxy :: Proxy (Ptr a))
+  paEitherLog "PA Available Devices" . liftIO $ [C.block| long {
     PaDeviceIndex numDevices = Pa_GetDeviceCount();
     if (numDevices < 0) {
-      *$(long* ptrErrorCode) = numDevices;
-      return 1;
+      return numDevices; /* errCode == numDevices */
     } else {
-      PaDeviceInfo* deviceInfo = (PaDeviceInfo*) malloc(sizeof(PaDeviceInfo));
-      $(char** ptrDeviceNames) = (char**) malloc(numDevices * sizeof(char*));
-      char** deviceNames = $(char** ptrDeviceNames);
+      PaDeviceInfo** deviceList = $(void** ptrDeviceList');
+      PaDeviceInfo** deviceList = (PaDeviceInfo**) malloc(numDevices * sizeof(PaDeviceInfo*));
       for (size_t i=0; i<numDevices; i++) {
-        deviceInfo = Pa_GetDeviceInfo(i);
-        size_t nameLength = strlen(deviceInfo->name) + 1;
-        *(deviceNames + i) = (char*) malloc(nameLength * sizeof(char));
-        strcpy(*(deviceNames + i), deviceInfo->name);
+        *(deviceList + i) = (PaDeviceInfo*) malloc(sizeof(PaDeviceInfo));
+        *(deviceList + i) = Pa_GetDeviceInfo(i);
       }
-      return numDevices;
-
-
-
-  ptrErrorCode <- pmMalloc :: Ptr' CLong
-  ptrDeviceNames <- pmMalloc :: Ptr' (Ptr CChar)
-  cResult <- liftIO $ [C.block| int {
-    void phantom_error_handler(const char* file, int line, const char* fun,
-                               int err, const char* fmt,...) { ; };
-    snd_lib_error_set_handler(&phantom_error_handler);
-    Pa_Initialize();
-    PaDeviceIndex numDevices = Pa_GetDeviceCount();
-    if (numDevices < 0) {
-      *$(long* ptrErrorCode) = numDevices;
-      return 1;
-    } else {
-      PaDeviceInfo* deviceInfo = (PaDeviceInfo*) malloc(sizeof(PaDeviceInfo));
-      $(char** ptrDeviceNames) = (char**) malloc(numDevices * sizeof(char*));
-      char** deviceNames = $(char** ptrDeviceNames);
-      for (size_t i=0; i<numDevices; i++) {
-        deviceInfo = Pa_GetDeviceInfo(i);
-        size_t nameLength = strlen(deviceInfo->name) + 1;
-        *(deviceNames + i) = (char*) malloc(nameLength * sizeof(char));
-        strcpy(*(deviceNames + i), deviceInfo->name);
-      }
-      return numDevices;
     }
+    return 0;
   } |]
-  liftIO $ print cResult
-  result <- if (fromIntegral cResult == 1)
-              then Left <$> obtainPAError ptrErrorCode
-              else let numDevices = fromIntegral cResult
-                   in Right <$> buildDevicesList ptrDeviceNames numDevices
-  r <- pmFreeAll
-  liftIO $ print r
-  return result;
-  where obtainPAError :: Ptr CLong -> HeapPM PortAudioError
-        obtainPAError ptrErrorCode = do
-          ptrErrorText <- pmMalloc :: Ptr' CChar
-          _ <- liftIO $ [C.block| int {
-            strcpy($(char* ptrErrorText), Pa_GetErrorText(*$(long* ptrErrorCode)));
-            return 0;
-          } |]
-          errorText <- liftIO $ C.peekCAString ptrErrorText
-          errorCode' <- liftIO $ [C.block| int { return *$(long* ptrErrorCode); } |]
-          let errorCode = fromIntegral errorCode' :: Int
-          return PortAudioError{..}
-
-        buildDevicesList :: Ptr (Ptr CChar) -> Int -> HeapPM DevicesList
-        buildDevicesList _ 0 = return []
-        buildDevicesList ptrDeviceNames numDevices = do
-          ptrDeviceName <- pmMalloc :: Ptr' CChar
-          liftIO $ [C.block| int {
-            size_t nameLength = strlen(*$(char** ptrDeviceNames)) + 1;
-            $(char* ptrDeviceName) = (char*) malloc(nameLength * sizeof(char));
-            strcpy($(char* ptrDeviceName), *$(char** ptrDeviceNames));
-            return 0;
-          } |]
-          liftIO $ C.peekCAString ptrDeviceName >>= print
-          device <- liftIO (C.peekCAString ptrDeviceName) >>= return . Device
-          let s = sizeOf ptrDeviceNames
-          restDevices <- buildDevicesList (Ptr.plusPtr ptrDeviceNames s) (pred numDevices)
-          return $ device:restDevices
-
-resTest :: ResIO ()
-resTest = do
-  allocate malloc free
+  let ptrDeviceList = castPtr ptrDeviceList' :: Ptr (Ptr PaDeviceInfo)
+  deviceList <- liftIO (peek ptrDeviceList >>= peek)
+  return [deviceList]
 
 main :: IO ()
-main = do
-  heapPointers <- newIORef []
-  result <- runReaderT audioDevicesAvailable heapPointers
-  print result
+main = print =<< (runResourceT $ do
+  paEitherLog "PA Init" initPa
+  r <- audioDevicesAvailable
+  return r)
+  where initPa = liftIO $ [C.block| long {
+          void phantom_error_handler(const char* file, int line, const char* fun,
+                                     int err, const char* fmt,...) { ; };
+          snd_lib_error_set_handler(&phantom_error_handler);
+          return 0;
+          err = Pa_Initialize();
+          if (err != paNoError) {
+            return err;
+          }
+        } |]
--- src/Types.hs
+++ src/Types.hs
@@ -1,24 +1,36 @@
-{-# LANGUAGE StrictData, DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE StrictData, DeriveGeneric    #-}
 
 module Types
   ( PaDeviceInfo(..), PaDeviceList
-  , PaAudioError(..)
+  , PaAudioError(..), paErrorByCode, paEitherLog
   ) where
 
-import           GHC.Generics             (Generic)
-import           Foreign.C.Types          (CInt, CDouble)
-import           Foreign.C.String         (CString)
+import qualified Language.C.Inline       as C
+import           GHC.Generics                 (Generic)
+import           Foreign.C.Types              (CInt, CLong, CDouble, CChar)
+import           Foreign.C.String             (CString)
+import qualified Foreign.C.String        as C (peekCAString)
 import           Foreign.Storable.Generic
-import           Control.Monad.Logger     (ToLogStr(..))
-import           Data.String              (IsString(..))
+import           Control.Monad                (void)
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Logger         (ToLogStr(..), LogLevel(..), MonadLogger(..), LogSource)
+import qualified Control.Monad.Logger    as L (logWithoutLoc)
+import           Control.Monad.Trans.Resource (ResourceT, MonadResource)
+import           Control.Exception            (Exception(..))
+import qualified Control.Exception       as E (throwIO)
+import           Data.String                  (IsString(..))
+import           Data.Proxy                   (Proxy(Proxy))
+import           Internal.Foreign             (malloc)
 
-type PaHostApiIndex = CInt
-type PaTime         = CDouble
+type PaHostApiIndex     = CInt
+type PaTime             = CDouble
+type PaDeviceList       = [PaDeviceInfo]
 
-type PaDeviceList = [PaDeviceInfo]
 
 data PaDeviceInfo = PaDeviceInfo
-  { padName                     :: CString
+  { _structVersion              :: CInt
+  , padName                     :: CString
   , padAudioApi                 :: PaHostApiIndex
   , padMaxInputChannels         :: CInt
   , padMaxOutputChannels        :: CInt
@@ -31,8 +43,34 @@ data PaDeviceInfo = PaDeviceInfo
 
 instance GStorable PaDeviceInfo
 
-data PaAudioError = PaAudioError Int String
 
+data PaAudioError = PaAudioError Int String
+  deriving (Show)
+
+instance Exception PaAudioError
 instance ToLogStr PaAudioError where
   toLogStr (PaAudioError errCode errText) = fromString $
     "PaError ~ code " ++ show errCode ++ ", " ++ errText
+
+paErrorByCode :: (MonadResource m) => CLong -> ResourceT m PaAudioError
+paErrorByCode errCode = do
+  (_, ptrErrText) <- malloc (Proxy :: Proxy CChar)
+  liftIO $ do
+    copyErrorText ptrErrText errCode
+    errText <- C.peekCAString ptrErrText
+    return $ PaAudioError (fromIntegral errCode) errText
+  where copyErrorText ptr errCode = void $ [C.block| int {
+          strcpy($(char* ptr), Pa_GetErrorText($(long errCode)));
+          return 0;
+        } |]
+
+paEitherLog :: (MonadResource m, MonadLogger m) => LogSource -> ResourceT m CLong -> ResourceT m ()
+paEitherLog logSource action = action >>= \result ->
+  if ((toEnum . fromIntegral) result :: Bool)
+    then let errCode = result
+         in do
+           paError <- paErrorByCode errCode
+           L.logWithoutLoc logSource LevelError paError
+           liftIO . void $ E.throwIO paError
+           return ()
+    else return ()
